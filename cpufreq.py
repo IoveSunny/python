2d2f071e244afec23a2485bc32b07c162de41497
SunnyPotter <huangjinhua4535@gmail.com>
2014-01-22 23:58:02 +0800

    Use cpufreq* control CPU Frequence in Ubuntu

--- cpufreq.py
+++ cpufreq.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 #coding:utf-8
 
+
 from os import popen
 
 for i in range(4):
 	global info
+	# Ubuntu cpufreq* 来控制 cpu frequence（CPU频率）
 	popen("cpufreq-set -c "+str(i)+" -d 800M && cpufreq-set -c "+str(i)+" -u 1500M")
 
 print popen("cpufreq-info").read()
